072ae18fc56c6d356de00289a2971e6fb36807c8
Tsingying XU <tsingying@xu>
2024-09-25 01:42:57 +1000

    finished language primer

--- server/server.py
+++ server/server.py
@@ -4,25 +4,35 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+groups = []
+students = [
+        {"id": 1, "name": "Alice"},
+        {"id": 2, "name": "Bob"},
+        {"id": 3, "name": "Charlie"},
+        {"id": 4, "name": "David"},
+        {"id": 5, "name": "Eve"},
+    ]
+group_id_counter = 0
+
+def find_student_by_id(sid):
+    for student in students:
+        if student["id"] == int(sid):
+            return student
+    return None
+
+def find_student_by_name(sname):
+    for student in students:
+        if student["name"] == sname:
+            return student
+    return None
+
 @app.route('/api/groups', methods=['GET'])
 def get_groups():
     """
     Route to get all groups
     return: Array of group objects
     """
-    # TODO: (sample response below)
-    return jsonify([
-        {
-            "id": 1,
-            "groupName": "Group 1",
-            "members": [1, 2, 3],
-        },
-        {
-            "id": 2,
-            "groupName": "Group 2",
-            "members": [4, 5],
-        },
-    ])
+    return groups
 
 @app.route('/api/students', methods=['GET'])
 def get_students():
@@ -30,14 +40,7 @@ def get_students():
     Route to get all students
     return: Array of student objects
     """
-    # TODO: (sample response below)
-    return jsonify([
-        {"id": 1, "name": "Alice"},
-        {"id": 2, "name": "Bob"},
-        {"id": 3, "name": "Charlie"},
-        {"id": 4, "name": "David"},
-        {"id": 5, "name": "Eve"},
-    ])
+    return students
 
 @app.route('/api/groups', methods=['POST'])
 def create_group():
@@ -52,14 +55,26 @@ def create_group():
     group_data = request.json
     group_name = group_data.get("groupName")
     group_members = group_data.get("members")
-    
-    # TODO: implement storage of a new group and return their info (sample response below)
+    group_member_ids = []
+    for name in group_members:
+        stripped_name = name.strip()
+        student = find_student_by_name(stripped_name)
+        if student is None:
+            return "Student " + stripped_name + " not found", 400
+        group_member_ids.append(student["id"])
 
-    return jsonify({
-        "id": 3,
-        "groupName": "New Group",
-        "members": [1, 2],
-    }), 201
+    global group_id_counter
+    group_id_counter += 1
+    group_id = group_id_counter
+
+    group_obj = {
+        "id": group_id,
+        "groupName": group_name,
+        "members": group_member_ids,
+    }
+
+    groups.append(group_obj)
+    return jsonify(group_obj), 201
 
 @app.route('/api/groups/<int:group_id>', methods=['DELETE'])
 def delete_group(group_id):
@@ -68,9 +83,12 @@ def delete_group(group_id):
     param group_id: The ID of the group to delete
     return: Empty response with status code 204
     """
-    # TODO: (delete the group with the specified id)
+    for group in groups:
+        if group["id"] == group_id:
+            groups.remove(group)
+            return '', 204
 
-    return '', 204  # Return 204 (do not modify this line)
+    return "Group id " + group_id + " is not found", 400
 
 @app.route('/api/groups/<int:group_id>', methods=['GET'])
 def get_group(group_id):
@@ -80,18 +98,14 @@ def get_group(group_id):
     return: The group object with member details
     """
     # TODO: (sample response below)
-    return jsonify({
-        "id": 1,
-        "groupName": "Group 1",
-        "members": [
-            {"id": 1, "name": "Alice"},
-            {"id": 2, "name": "Bob"},
-            {"id": 3, "name": "Charlie"},
-        ],
-    })
-    # TODO:
-    # if group id isn't valid:
-    #     abort(404, "Group not found")
+    for group in groups:
+        if group["id"] == group_id:
+            return jsonify({
+                    "id": group["id"],
+                    "groupName": group["groupName"],
+                    "members": list(map(find_student_by_id, group["members"])),
+                })
+    abort(404, "Group not found")
 
 if __name__ == '__main__':
     app.run(port=3902, debug=True)
